9d2809f3cfcfa7041f32338dee24545826767b0a
Nikodem <105146265+nilkoxd@users.noreply.github.com>
2023-03-30 20:41:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+
+const { spawn } = require('child_process');
 const path = require('path');
+
+const bot = spawn('node', [path.join(__dirname, 'index.js')]);
+
+bot.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+bot.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+bot.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
 
 // Endpoint dla bota
 app.get('/bot', (req, res) => {
